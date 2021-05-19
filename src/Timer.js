75e13a2059b5a994e5b74d17810131e1dad37448
Nahum <losond8@gmail.com>
2021-05-18 18:27:12 -0700

    Implementing component lifescycle methods.

--- src/Timer.js
+++ src/Timer.js
@@ -7,6 +7,13 @@ class Timer extends Component {
   };
 
   // add your code here
+  componentDidMount(){
+    this.clockTick()
+  }
+
+  componentWillUnmount(){
+    this.stopClock()
+  }
 
   render() {
     const { time, color } = this.state;
